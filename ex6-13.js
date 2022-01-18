feda70b4f3a3486702838bfba12d19d23cf14879
Osamu Sugimoto <sam@josai.ac.jp>
2022-01-18 00:25:11 +0000

    0118

---
+++ ex6-13.js
@@ -0,0 +1,18 @@
+function ChangeImageAndBack(){
+  
+  imgback();
+  
+  ThreeSecChange2(); 
+  
+}
+
+function ThreeSecChange2(){
+  setTimeout("imgchange2()",3000);
+}
+function imgchange2() {
+  document.getElementById('img13').src = './image/josai.png';
+}
+
+function imgback() {
+  document.getElementById('img13').src = './image/bulbon.gif';
+}
